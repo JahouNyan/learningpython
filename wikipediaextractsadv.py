b1483b376cb3ebdb7102106cd060cfbeddb706ca
Jahou Nyan <jahounyan@gmail.com>
2018-10-11 13:01:11 +0200

    Update wikipediaextractsadv.py

--- wikipediaextractsadv.py
+++ wikipediaextractsadv.py
@@ -2,46 +2,45 @@
 import requests
 import json
 
-
 #Ask the user for an article and strip it and replaces the spaces with underscores (_)
 article = input("What wikipedia article do you want? ")
 article = article.strip().replace(" ", "_")
-language = input("select your language: en, af or fr: ")
-
-
 # list language choices
-en = "https://en.wikipedia.org/api/rest_v1/page/summary/"
-af = "https://af.wikipedia.org/api/rest_v1/page/summary/"
-fr = "https://fr.wikipedia.org/api/rest_v1/page/summary/"
+languages = {
+    "en" : "English",
+    "af" : "Afrikaans",
+    "fr" : "French"
+}
 
-#Format the API endpoint with the article
-searcharticle = (language) + article
-#Use requests.get() to get the data
-r = requests.get(searcharticle)
-print(r)
+for lang, langname in languages.items():
+    print(f"{article.capitalize()} on {langname} wikipedia page:")
+    #Format the API endpoint with the article
+    url = f"https://{lang}.wikipedia.org/api/rest_v1/page/summary/{article}"
+    #Use requests.get() to get the data
+    r = requests.get(url)
+    
+    
 
-#Check if we got a 200 status code, otherwise abort the program
-if r.status_code is 200:
-    print("ok")
-        
-else:
-    print ("error")
-    exit()
+    #Check if we got a 200 status code, otherwise abort the program
+    if r.status_code is 200:
+        print("the url status is ok.")
+            
+    else:
+        print ("error")
+        exit()
+    
+    #Display the title of the article, description and extract
+    lines = r.text
+    lines = json.loads(lines)    
+    title = lines["title"]
+    extract = lines["extract"]
+    print(title)
+    #some articles don't have descriptions so:
+    #description = lines["description"]
+    if 'description' not in lines:
+        print ("No Description")
+    else:
+        print(f"The description on wikipedia is: {lines['description']}")
+    print(f"The extract is: {extract}")
 
-#Display the title of the article, description and extract
-lines = r.text
-lines = json.loads(lines)
-
-title = lines["title"]
-description = lines["description"]
-extract = lines["extract"]
-print(title)
-#some articles don't habe descriptions so:
-if description not in lines:
-    print ("No Description")
-else:
-    print(f"The description on wikipedia is: {description}")
-print(f"The extract is: {extract}")
-
-#show the description and extract in three languages of your choice e.g 'en', 'es', 'nl', etc.)
 
